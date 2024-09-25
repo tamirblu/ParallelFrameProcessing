c27a4f28978be80b80b0699b227922a0028b6abf
tamirblu <tamirblumberg@gmail.com>
2024-09-25 21:39:02 +0300

    refactor

---
+++ src/FrameCapture.h
@@ -0,0 +1,42 @@
+#ifndef FRAME_CAPTURE_H
+#define FRAME_CAPTURE_H
+
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <thread>
+#include <atomic>
+#include <vector>
+#include <functional> // Include functional header
+#include "Module.h"
+
+class FrameCapture {
+public:
+//  Constructor :  explicit: Prevents implicit conversions, ensuring the constructor is called only with a std::string.
+    explicit FrameCapture(const std::string& videoFilePath);
+//Destructor (~FrameCapture()): Ensures resources are cleaned up when the object is destroyed.
+    ~FrameCapture();
+
+//    Control the lifecycle of the frame capture thread.
+    void join();
+    void start();
+    void stop();
+
+    // Register processing modules
+    //    Allows registration of processing modules using std::function, which can store lambdas, function pointers, or bind expressions.
+//    void addModule(std::function<void(const cv::Mat&, int)> moduleFunc);
+    void addModule(Module* module);
+
+private:
+    void captureLoop();
+    //    std::string videoFilePath_: Stores the path to the video file.
+    //std::thread captureThread_: Thread for capturing frames.
+    //std::atomic<bool> running_: Atomic flag to control the running state of the thread safely across multiple threads.
+    //std::vector<std::function<void(const cv::Mat&, int)>> modules_: Stores the registered processing modules.
+    std::string videoFilePath_;
+    std::thread captureThread_;
+    std::atomic<bool> running_;
+    std::vector<Module*> modules_;
+
+};
+
+#endif // FRAME_CAPTURE_H
---
+++ src/SlowModule.cpp
@@ -0,0 +1,93 @@
+#include "SlowModule.h"
+#include <iostream>
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem; // For convenience
+
+SlowModule::SlowModule(int sleepPeriodMs)
+        : running_(false), sleepPeriodMs_(sleepPeriodMs) {
+    // Create the directory if it doesn't exist
+    fs::path rootDirectory = fs::current_path().parent_path().parent_path();
+    std::string root = rootDirectory.string();
+    const std::string directoryPath = root + "/Frames";
+
+    if (!fs::exists(directoryPath)) {
+        fs::create_directory(directoryPath);
+        std::cout << "[SlowModule] Created directory: " << directoryPath << std::endl;
+    }
+    // Store the directory path for later use
+    saveDirectory_ = directoryPath;
+}
+
+SlowModule::~SlowModule() {
+    stop();
+}
+
+void SlowModule::start() {
+    running_ = true;
+    processingThread_ = std::thread(&SlowModule::processingLoop, this);
+}
+
+void SlowModule::stop() {
+    if (running_) {
+        running_ = false;
+        cv_.notify_all();
+        if (processingThread_.joinable()) {
+            processingThread_.join();
+        }
+    }
+}
+
+
+void SlowModule::processFrame(const cv::Mat& frame, int frameNumber) {
+    std::lock_guard<std::mutex> lock(queueMutex_);
+
+    if (frameQueue_.size() >= maxQueueSize_) {
+        // Remove and discard the oldest frame
+        int droppedFrameNumber = frameQueue_.front().second;
+        frameQueue_.pop_front();
+
+        journal_.logDroppedFrame(droppedFrameNumber);
+//        std::cout << "[SlowModule] Dropped frame number: " << droppedFrameNumber << std::endl;
+    }
+
+    frameQueue_.emplace_back(frame, frameNumber);
+    cv_.notify_one();
+}
+
+void SlowModule::processingLoop() {
+    while (running_ || !frameQueue_.empty()) {
+        std::unique_lock<std::mutex> lock(queueMutex_);
+        cv_.wait(lock, [this] { return !frameQueue_.empty() || !running_; });
+
+        if ((!running_ && frameQueue_.empty()) || ( frameQueueLen_>frameQueue_.size())) {
+            break;
+        }
+
+        auto frameData = frameQueue_.front();
+//        popSpecificFrame(frameQueue_.front().second);
+        frameQueue_.pop_front();
+        lock.unlock();
+
+        // Save the frame as a JPEG file
+        std::string filename = saveDirectory_ + "/frame_" + std::to_string(frameData.second) + ".jpeg";
+        bool success = cv::imwrite(filename, frameData.first);
+
+        if (success) {
+            journal_.logSavedFrame(frameData.second);
+            std::cout << "[SlowModule] Saved frame number: " << frameData.second << std::endl;
+        } else {
+            std::cerr << "[SlowModule] Error saving frame number: " << frameData.second << std::endl;
+        }
+
+        // Sleep for the defined period
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleepPeriodMs_));
+        if (frameQueueLen_ < frameQueue_.size()){
+            frameQueueLen_++;
+        }
+    }
+}
+
+void SlowModule::printJournalSummary() {
+    journal_.printSummary();
+}
